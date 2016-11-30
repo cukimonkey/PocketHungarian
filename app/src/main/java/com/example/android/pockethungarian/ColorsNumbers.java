b1ed85003c8b2a69784193778c4a875714c5d241
Orsi <cukimonkey@gmail.com>
2016-11-30 19:27:04 +1300

    Creating secondary activities

---
+++ app/src/main/java/com/example/android/pockethungarian/ColorsNumbers.java
@@ -0,0 +1,13 @@
+package com.example.android.pockethungarian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColorsNumbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors_numbers);
+    }
+}
---
+++ app/src/main/java/com/example/android/pockethungarian/Grocery.java
@@ -0,0 +1,13 @@
+package com.example.android.pockethungarian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Grocery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grocery);
+    }
+}
